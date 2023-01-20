8fcadd57d135b32d895ea69e683c90bd9ce49f7f
GioGio4 <g.ceccobelli@outlook.it>
2023-01-20 16:08:52 +0100

    Add Variables

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+// VARIABILI 
+
+const numero_fisso = "23";
+// console.log(typeof numero_fisso);
+
+let nome = prompt("Il tuo nome?");
+// console.log(nome);
+
+let cognome = prompt("Il tuo cognome?")
+// console.log(cognome);
+
+let colore = prompt("Il tuo colore preferito?")
+// console.log(typeof colore);
+
+
+
+
+
+
 
